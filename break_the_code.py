5597e5146b1157e8f90a84ef16d2248e8e7adeaa
niki843 <niki.tornev96@gmail.com>
2022-12-31 14:38:19 +0200

    added new background img and removed some upgraded requirements

--- break_the_code.py
+++ break_the_code.py
@@ -10,20 +10,19 @@ def start_game():
     pygame.init()
 
     # pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-    screen = pygame.display.set_mode((800, 600), pygame.HWSURFACE | pygame.DOUBLEBUF | pygame.RESIZABLE)
+    screen = pygame.display.set_mode((1280, 720), pygame.HWSURFACE | pygame.DOUBLEBUF | pygame.RESIZABLE)
     pygame.display.set_caption("Break The Code")
     thumbnail = pygame.image.load(
         f"{img_path}crack-the-code-thumbnail.png"
     )
     pygame.display.set_icon(thumbnail)
-    background = pygame.image.load(f"{img_path}background.jpg")
-    screen.blit(pygame.transform.scale(background, (800, 600)), (0, 0))
+    background = pygame.image.load(f"{img_path}background.png")
+    screen.blit(pygame.transform.scale(background, (1280, 720)), (0, 0))
     pygame.display.flip()
 
     running = True
     while running:
         running = check_events(background, screen) is None
-
         pygame.display.update()
 
 
