bfd563acb3d86cb1c39d9fd016ae27e3efd7ee8d
niki843 <niki.tornev96@gmail.com>
2023-02-10 16:04:03 +0200

    fixed background for pygame

--- break_the_code.py
+++ break_the_code.py
@@ -1,6 +1,5 @@
 import pygame
 import os
-
 from utils.game_events import check_events
 
 img_path = f".{os.path.sep}images{os.path.sep}"
@@ -16,6 +15,7 @@ def start_game():
         f"{img_path}crack-the-code-thumbnail.png"
     )
     pygame.display.set_icon(thumbnail)
+
     background = pygame.image.load(f"{img_path}background.png")
     screen.blit(pygame.transform.scale(background, (1280, 720)), (0, 0))
     pygame.display.flip()
