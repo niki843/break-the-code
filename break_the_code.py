364edc2dded80722a7318ab6a0478496a2049a0d
niki843 <niki.tornev96@gmail.com>
2023-02-10 18:20:34 +0200

    Added recieve server message and send server message in threads

--- break_the_code.py
+++ break_the_code.py
@@ -1,12 +1,47 @@
 import pygame
 import os
-from utils.game_events import check_events
+import asyncio
+import threading
+
+from client import ws_client as client
 
 img_path = f".{os.path.sep}images{os.path.sep}"
 
 
+ws_client = client.WebsocketClient()
+
+
+def start_server(loop, future):
+    loop.run_until_complete(ws_client.connect_to_server())
+
+
+def send_message(message, future):
+    print(message)
+    asyncio.run(ws_client.send_server_messages('{"type": "new_game", "player_id": "0ed61f48-2761-4c82-9460-788af1f52795", "player_name": "first_player"}'))
+
+
+def stop_server(loop, future):
+    loop.call_soon_threadsafe(future.set_result, None)
+
+
 def start_game():
+    # Create a server client and connect to the server
+    loop = asyncio.get_event_loop()
+    future = loop.create_future()
+    thread = threading.Thread(target=start_server, args=(loop, future))
+    thread.start()
+
     pygame.init()
+    # screen dimensions
+    HEIGHT = 320
+    WIDTH = 480
+
+    # set up the drawing window
+    screen = pygame.display.set_mode([WIDTH, HEIGHT])
+
+    color = pygame.Color('blue')
+    radius = 30
+    x = int(WIDTH / 2)
 
     # pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
     screen = pygame.display.set_mode((1280, 720), pygame.HWSURFACE | pygame.DOUBLEBUF | pygame.RESIZABLE)
@@ -22,8 +57,30 @@ def start_game():
 
     running = True
     while running:
-        running = check_events(background, screen) is None
-        pygame.display.update()
+        # did the user close the window
+        for event in pygame.fastevent.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == client.EVENT_TYPE:
+                print(event.message)
+                color = pygame.Color('red')
+                x = (x + radius / 3) % (WIDTH - radius * 2) + radius
+            elif event.type == pygame.KEYDOWN:
+                color = pygame.Color('red')
+                x = (x + radius / 3) % (WIDTH - radius * 2) + radius
+                thread = threading.Thread(target=send_message, args=("test", future))
+                thread.start()
+
+    end_game(loop, future, thread)
+
+
+def end_game(loop, future, thread):
+    print("Stoping event loop")
+    stop_server(loop, future)
+    print("Waiting for termination")
+    thread.join()
+    print("Shutdown pygame")
+    pygame.quit()
 
 
 if __name__ == "__main__":
--- client/ws_client.py
+++ client/ws_client.py
@@ -4,37 +4,42 @@ import asyncio
 import aioconsole
 
 import websockets
+import pygame
 
 
 # Initial connection to server throw exception if an error occurs
 from websockets.exceptions import ConnectionClosed
 
+IPADDRESS = "localhost"
+PORT = "8765"
 
-async def connect_to_server():
-    try:
-        async with websockets.connect("ws://localhost:8765") as websocket:
-            await asyncio.gather(wait_for_server_message(websocket), send_server_message(websocket))
-
-    except ConnectionRefusedError:
-        print("Could not connect to server")
-    except Exception:
-        print("An unknown exception occurred")
+EVENT_TYPE = pygame.event.custom_type()
 
 
-async def wait_for_server_message(websocket):
-    async for message in websocket:
+class WebsocketClient:
+    def __init__(self):
+        self.websocket = None
+
+    async def connect_to_server(self):
         try:
-            print(message)
-        except ConnectionClosed:
-            print("The server closed the connection")
+            async with websockets.connect(f"ws://{IPADDRESS}:{PORT}") as websocket:
+                self.websocket = websocket
+                await self.wait_for_server_message()
+
+        except ConnectionRefusedError:
+            print("Could not connect to server")
+
+    async def wait_for_server_message(self):
+        if not self.websocket:
+            raise Exception("No active connection!")
+        async for message in self.websocket:
+            print(f"[Received]: {message}")
+            pygame.fastevent.post(pygame.event.Event(EVENT_TYPE, message=message))
+
+    async def send_server_messages(self, message):
+        await self.websocket.send(message)
 
 
-async def send_server_message(websocket):
-    inp = await aioconsole.ainput('json: ')
-    while inp != "end":
-        await websocket.send(inp)
-        inp = await aioconsole.ainput('json: ')
-
-
-if __name__ == "__main__":
-    asyncio.run(connect_to_server())
+# if __name__ == "__main__":
+#     ws_client = WebsocketClient()
+#     asyncio.run(ws_client.connect_to_server())
