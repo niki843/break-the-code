ea262da155aed0bb1d9b8da21d3aaf7fa3c2dc93
niki843 <niki.tornev96@gmail.com>
2023-03-16 11:31:48 +0200

    Created slider class and added function to track percentage

---
+++ client/game_objects/tiles/slider.py
@@ -0,0 +1,38 @@
+from client.game_objects.tiles.tile import Tile
+
+
+class Slider(Tile):
+    def __init__(
+        self,
+        name,
+        surface,
+        screen,
+        size_percent,
+        tile_addition_width,
+        tile_addition_height,
+        handle_name,
+        handle_surface,
+        handle_size_percent,
+    ):
+        super().__init__(
+            name,
+            surface,
+            screen,
+            size_percent,
+            tile_addition_width,
+            tile_addition_height,
+        )
+        self.slider_handle = Tile(
+            handle_name, handle_surface, screen, handle_size_percent, 0, 0
+        )
+
+        self.slider_percentage = 0
+
+    def set_slider_percentage(self):
+        self.slider_percentage = int(
+            (
+                (self.slider_handle.rect.centerx - self.rect.left)
+                / self.image.get_width()
+            )
+            * 100
+        )
