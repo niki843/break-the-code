085175faa106f4a9d1aca7005b09720a5f654658
niki843 <niki.tornev96@gmail.com>
2023-04-06 14:29:42 +0300

    Added messages to the cards that will be shown as popups over the player icons

--- client/game_objects/entities/condition_card.py
+++ client/game_objects/entities/condition_card.py
@@ -2,9 +2,11 @@ from types import SimpleNamespace
 
 
 class ConditionCard(SimpleNamespace):
-    def __init__(self, id=None, description=None, additional_info=None, **kwargs):
+    def __init__(self, id=None, description=None, additional_info=None, positive_condition_message=None, negative_condition_message=None, **kwargs):
         self.id = id
         self.description = description
         self.additional_info = additional_info
+        self.positive_condition_message = positive_condition_message
+        self.negative_condition_message = negative_condition_message
 
         super().__init__(**kwargs)
