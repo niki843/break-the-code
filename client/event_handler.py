3e2339ebcc9af90465e941087446893f454e311e
niki843 <niki.tornev96@gmail.com>
2023-04-27 19:08:33 +0300

    Fixed ctrl alt fullscreen remove for testing

--- client/event_handler.py
+++ client/event_handler.py
@@ -176,7 +176,7 @@ class EventHandler(Singleton):
             getattr(self.current_window, window_function)(**message)
 
     def open_full_screen(self):
-        screen = pygame.display.set_mode((1366, 768), pygame.HWSURFACE)
+        screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         self.change_screen(screen)
         self.settings.resolution_slider.handle_position = len(self.settings.SCREEN_SIZE_CAPTIONS) - 1
         self.settings.resolution_slider.set_slider_handle_position()
