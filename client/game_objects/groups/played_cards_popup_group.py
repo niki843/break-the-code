1e54ed04d0bd05238601abca218a66b4c54dfece
niki843 <niki.tornev96@gmail.com>
2023-04-28 19:52:52 +0300

    Fixed scrolling to go through elements

--- client/game_objects/groups/played_cards_popup_group.py
+++ client/game_objects/groups/played_cards_popup_group.py
@@ -1,5 +1,9 @@
+import math
 import client
-from client.game_objects.tiles.height_resizable_multiline_text_tile import HeightResizableMultilineTextTile
+
+from client.game_objects.tiles.height_resizable_multiline_text_tile import (
+    HeightResizableMultilineTextTile,
+)
 from client.game_objects.tiles.slider import Slider
 
 from client.game_objects.tiles.tile import Tile
@@ -21,21 +25,24 @@ class PlayedCardsPopupGroup:
 
         self.is_open = False
 
+        self.full_size_px = 0
+        self.single_scroll_px = 0
+
         self.build()
 
     def add_played_card(self, played_card_tile: Tile, player_responses: dict):
         player_response_tiles = []
         for name, response in player_responses.items():
-            player_response_tiles.append(
-                HeightResizableMultilineTextTile(
-                    name="player_response",
-                    screen=client.state_manager.screen,
-                    width_percent=35,
-                    text_to_display=f"{name}: {response}",
-                    text_size_percent=5,
-                    start_line=0
-                )
+            player_response_tile = HeightResizableMultilineTextTile(
+                name="player_response",
+                screen=client.state_manager.screen,
+                width_percent=35,
+                text_to_display=f"{name}: {response}",
+                text_size_percent=5,
+                start_line=0,
             )
+
+            player_response_tiles.append(player_response_tile)
 
         self.played_cards_and_player_responses.append(
             (played_card_tile, player_response_tiles)
@@ -43,6 +50,11 @@ class PlayedCardsPopupGroup:
 
         played_card_tile.size_percent = 23
         played_card_tile.resize()
+
+        self.full_size_px += played_card_tile.image.get_height()
+
+        if self.full_size_px > self.background.image.get_height():
+            self.scroll.update(delimiters=math.ceil((self.full_size_px - self.background.image.get_height()) / self.single_scroll_px) + 1)
 
         self.set_player_responses_size(played_card_tile, player_response_tiles)
 
@@ -53,12 +65,12 @@ class PlayedCardsPopupGroup:
             card.rect.centerx = self.background.rect.centerx
             card.rect.top = top
 
+            self.full_size_px += card.image.get_height()
+
             top = self.set_player_responses_size(card, player_responses)
 
     def set_player_responses_size(self, card_tile, response_tiles):
-        top = card_tile.rect.bottom + (
-            self.background.image.get_height() * 0.02
-        )
+        top = card_tile.rect.bottom + (self.background.image.get_height() * 0.02)
         for response_tile in response_tiles:
             response_tile.resize()
             response_tile.rect.left = self.background.rect.left + (
@@ -70,6 +82,8 @@ class PlayedCardsPopupGroup:
             top = response_tile.rect.bottom + (
                 self.background.image.get_height() * 0.02
             )
+
+            self.full_size_px += response_tile.image.get_height()
         return top
 
     def build(self):
@@ -78,6 +92,7 @@ class PlayedCardsPopupGroup:
         self.build_scroll()
 
     def resize(self):
+        self.full_size_px = 0
         self.set_background_size()
         self.set_played_cards_button_size()
         self.set_played_cards_size()
@@ -87,7 +102,6 @@ class PlayedCardsPopupGroup:
         self.close() if self.is_open else self.open()
 
     def open(self):
-
         self.is_open = True
 
         self.tiles_group.add(self.background)
@@ -124,6 +138,8 @@ class PlayedCardsPopupGroup:
         else:
             self.background.rect.left = client.state_manager.screen_rect.right
 
+        self.single_scroll_px = math.ceil(self.background.image.get_height() * common.get_percentage_multiplier_from_percentage(20))
+
     def build_played_cards_button(self):
         self.played_cards_button = common.load_rotated_right_tile(
             "played_cards_arrow",
@@ -154,11 +170,11 @@ class PlayedCardsPopupGroup:
             handle_name="game_session_slider_handle",
             handle_surface=common.get_image("slider_button.png"),
             handle_size_percent=1.1,
-            delimiters_count=3,
+            delimiters_count=0,
             handle_position=0,
             horizontal=False,
             tile_addition_width_percent=0.2,
-            tile_addition_height_percent=26
+            tile_addition_height_percent=26,
         )
 
         self.set_scroll_size()
@@ -174,9 +190,19 @@ class PlayedCardsPopupGroup:
 
     def scroll_up(self):
         self.scroll.previous_handle_position()
+        self.scroll_elements(self.single_scroll_px)
 
     def scroll_down(self):
         self.scroll.next_handle_position()
+        # Add negative scroll pixels to scroll down
+        self.scroll_elements(self.single_scroll_px * -1)
+
+    def scroll_elements(self, scroll_pixels):
+        for played_card, player_responses in self.played_cards_and_player_responses:
+            played_card.rect.top += scroll_pixels
+            for player_response in player_responses:
+                player_response.rect.top += scroll_pixels
+                player_response.center_text()
 
     def blit(self):
         client.state_manager.screen.blit(
